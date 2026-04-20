33f7a3776a676e27a614670924ddf0fbb51d28dd
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-04-20 11:44:21 -0300

    Criando a ConnectionFactory

--- loja-virtual-repository/src/TesteConexao.java
+++ loja-virtual-repository/src/TesteConexao.java
@@ -1,15 +1,14 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class TesteConexao {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection con = DriverManager.
-				getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.recuperarConexa();
 		
-	
+		IO.println("Fechando conexão!!");
 		
 		con.close();
 	}
