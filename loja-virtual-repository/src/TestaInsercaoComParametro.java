bb818f5a5ce574060f511a1891b65f1a55b4935a
Gabriel Florentino <gabrieleduflorentino@hotmail.com>
2026-04-20 13:43:18 -0300

    Controle de transação - Assumindo o controle

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -2,22 +2,33 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLInvalidAuthorizationSpecException;
 import java.sql.Statement;
+
+import javax.management.RuntimeErrorException;
 
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "Mouse'";
-		String descricao = "Mouse sem fio); delete from PRODUTO";
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexa();
+		connection.setAutoCommit(false);
 
 		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
 
+		adicionarVarial("SmartTv", "45 polegadas", stm);
+		adicionarVarial("Radio", "Radio de bateria", stm);
+	}
+
+	private static void adicionarVarial(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 
+//		if (nome.equals("Radio")) {
+//			throw new RuntimeException("Não foi possível adicionar produto");
+//		}
+		
 		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
